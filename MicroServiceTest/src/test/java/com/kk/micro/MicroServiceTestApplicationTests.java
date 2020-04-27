933c1014b938042f3210c0b8ed838945363c4a1a
Kishore <anandan.kishore@gmail.com>
2020-04-27 11:20:00 +0530

    Eureka client

---
+++ MicroServiceTest/src/test/java/com/kk/micro/MicroServiceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kk.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
